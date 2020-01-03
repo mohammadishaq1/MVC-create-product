55cbd578e06902c36e39f49b8d90e490d3ae85de
Mohammad Ishaq <info@ishaq.io>
2020-01-03 13:07:47 -0600

    Delete row

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -23,5 +23,60 @@ namespace p6.Controllers
             Product p = db.Products.Where(temp => temp.ProductID == id).FirstOrDefault();
             return View(p);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            db.Products.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        public ActionResult Edit(long id)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product currentProduct = db.Products.Where(temp => temp.ProductID == id).FirstOrDefault();
+            return View(currentProduct);
+        }
+
+        [HttpPost]
+        public ActionResult Edit( Product p)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product currentProduct = db.Products.Where(temp => temp.ProductID == p.ProductID).FirstOrDefault();
+            currentProduct.ProductName = p.ProductName;
+            currentProduct.Price = p.Price;
+            currentProduct.DateOfPurchase = p.DateOfPurchase;
+            currentProduct.CategoryID = p.CategoryID;
+            currentProduct.BrandID = p.BrandID;
+            currentProduct.AvailabilityStatus = p.AvailabilityStatus;
+            currentProduct.Active = p.Active;
+            db.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        public ActionResult Delete(long id)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product currentProduct = db.Products.Where(temp => temp.ProductID == id).FirstOrDefault();
+            return View(currentProduct);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(long id, Product p)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product currentProduct = db.Products.Where(temp => temp.ProductID == id).FirstOrDefault();
+            db.Products.Remove(currentProduct);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
     }
 }
